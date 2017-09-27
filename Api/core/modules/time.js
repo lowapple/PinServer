4ace06749144b8f204a7cedae27ad09594731835
slopcat99 <slopcat99@gmail.com>
2017-09-27 23:35:52 +0900

    add time

---
+++ Api/core/modules/time.js
@@ -0,0 +1,28 @@
+'use strict'
+
+function leadingZeros(n, digits) {
+    var zero = '';
+    n = n.toString();
+
+    if (n.length < digits) {
+        for (var i = 0; i < digits - n.length; i++) {
+            zero += '0';
+        }
+    }
+    return zero + n;
+}
+
+module.exports = {
+    getTime: () => {
+        var d = new Date();
+        var s =
+            leadingZeros(d.getFullYear(), 4).toString() +
+            leadingZeros(d.getMonth() + 1, 2).toString() +
+            leadingZeros(d.getDate(), 2).toString() +
+            leadingZeros(d.getHours(), 2).toString() +
+            leadingZeros(d.getMinutes(), 2).toString() +
+            leadingZeros(d.getSeconds(), 2).toString();
+
+        return s;
+    }
+}
