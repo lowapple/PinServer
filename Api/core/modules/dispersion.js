2a314c4767f292b4f0ba12ffc8a7657c37b99d62
slopcat99 <slopcat99@gmail.com>
2017-09-27 23:48:53 +0900

    add dispersion

--- Api/core/modules/dispersion.js
+++ Api/core/modules/dispersion.js
@@ -2,11 +2,23 @@
 
 var folder = require('./folderControl'),
     time = require('./time'),
-    config = require('../config');
+    config = require('../config'),
+    hash = require('./hash');
 
 module.exports = {
     dispersion: () => {
         var path = config.path.image,
-
+            folderName = hash.unidirectionalEncrypt(time.getTime(), 'md5');
+        return folder.getCountByFolder(path)
+            .then((result) => {
+                if (result == 0) {
+                    return folder.createFolders(path, [folderName])
+                        .then(() => {
+                            return path + '/' + folderName;
+                        });
+                } else {
+                    return path + '/' + folderName;
+                }
+            });
     }
 }
